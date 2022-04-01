f971a7d2c62260e466db3b912e7f82c02d2fef92
fernandocfbf <fernandocfbf@al.insper.edu.br>
2022-04-01 20:45:26 -0300

    feat: fixing error of two different lengths

--- alinhamento-local.cpp
+++ alinhamento-local.cpp
@@ -5,6 +5,8 @@
 #include <algorithm>
 #include <string> //to use string
 #include <random> //to use generator
+#include <stdio.h>
+#include <string.h>
 
 using namespace std;
 
@@ -27,7 +29,7 @@ void read_file(int &len_sequence1, int &len_sequence2, string &sequence1, string
 int generate_random_numbers(int min, int max);
 string generate_subsequence(string b, int j, int k);
 item run(string a, string b, int n, int m);
-string step1(string b, int m);
+string step1(int n, string b, int m);
 int step2();
 vector<string> step3(string a, int n, int p, int k);
 vector<item> step4(vector<string> subsequences, int vector_size, string sb);
@@ -41,36 +43,36 @@ int main(){
     string sequence1;
     string sequence2;
     read_file(len_sequence1, len_sequence2, sequence1, sequence2);
+    cout << endl;
     item best;
+    best.score = 0; //avoid memory trash
     for(int i=0; i<8; i++){
         item random = run(sequence1, sequence2, len_sequence1, len_sequence2);
-        cout << "CAlculated -> " << random.score << endl;
         if (random.score > best.score) {
             best.sa = random.sa;
             best.sb = random.sb;
             best.score = random.score;
         };
-        cout << "BEsT -> " << best.score << endl << endl;
-        cout << "BEsT -> " << best.sa << endl << endl;
-        cout << "BEsT -> " << best.sb << endl << endl;
     }
+
+    cout << best.sa << endl;
+    cout << best.sb << endl;
+    cout << best.score << endl;
+
     return 0;
 }
 
 item run(string a, string b, int n, int m){
-    string sb = step1(b, m);
+    string sb = step1(a.size(), b, m);
     int p = step2();
     vector<string> p_sa = step3(a, n, p, sb.size());
     vector<item> score_subsequences = step4(p_sa, p_sa.size(), sb);
     item best_alingment = step5(score_subsequences, score_subsequences.size());
-    cout << "best_alingment -> " << best_alingment.score << endl << endl;
-    cout << "best_alingment -> " << best_alingment.sa << endl << endl;
-    cout << "best_alingment -> " << best_alingment.sb << endl << endl;
     return best_alingment;
 }
 
-string step1(string b, int m){
-    int k = generate_random_numbers(1, m);
+string step1(int n, string b, int m){
+    int k = generate_random_numbers(1, n);
     int j = generate_random_numbers(0, m-k);
     return generate_subsequence(b, j, k);
 }
@@ -101,17 +103,15 @@ vector<item> step4(vector<string> subsequences, int vector_size, string sb){
 
 item step5(vector<item> results, int results_size){
     item best;
+    best.score = 0; //avoid memory trash
     for (int i=0; i<results_size; i++){
-        if(results[i].score > best.score){
-            best.score = results[i].score;
-            best.sa = results[i].sa;
-            best.sb = results[i].sb;
+        item element = results[i];
+        if(element.score > best.score){
+            best.score = element.score;
+            best.sa = element.sa;
+            best.sb = element.sb;
         } 
     }
-
-    cout << "step5 " << best.sa << endl;
-    cout << "step5 " << best.sb << endl;
-
     return best;
 }
 
